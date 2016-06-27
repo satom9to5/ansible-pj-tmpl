f86842703590a4e091a97c80d754fa70c62f301d
satom9to5 <satom9to5@gmail.com>
2016-06-27 17:36:17 +0900

    ansibleインストール＆実行時のバグ修正

--- rhel.ansible_install.sh
+++ rhel.ansible_install.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+set -e
+
 ANSIBLE_VERSION=2.1.0.0
 ANSIBLE_FULLNAME=ansible-${ANSIBLE_VERSION}
 
+[ `which` ] || yum install -y which
 [ `which wget` ] || yum install -y wget
 wget http://releases.ansible.com/ansible/${ANSIBLE_FULLNAME}.tar.gz -P /tmp/
 tar xvzf /tmp/${ANSIBLE_FULLNAME}.tar.gz -C /tmp/
 [ `which gcc` ] || yum install -y gcc
 if [ ! `which pip` ]; then
     yum install -y epel-release 
-    yum install -y python-pip
+    yum install -y python python-devel python-pip
+    pip install --upgrade pip setuptools
 fi
-yum install -y python-devel libffi-devel openssl-devel
+yum install -y libffi-devel openssl-devel 
 pip install /tmp/${ANSIBLE_FULLNAME}
 rm -rf /tmp/${ANSIBLE_FULLNAME}*
