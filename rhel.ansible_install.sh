2f700df5af3b0b3b8ce0b8b94b3ca2a0660626f6
satom9to5 <satom9to5@gmail.com>
2016-06-27 16:15:59 +0900

    ansibleインストール用ライブラリインストール追加

--- rhel.ansible_install.sh
+++ rhel.ansible_install.sh
@@ -8,8 +8,9 @@ wget http://releases.ansible.com/ansible/${ANSIBLE_FULLNAME}.tar.gz -P /tmp/
 tar xvzf /tmp/${ANSIBLE_FULLNAME}.tar.gz -C /tmp/
 [ `which gcc` ] || yum install -y gcc
 if [ ! `which pip` ]; then
-    yum install -y epel-release libffi-devel
+    yum install -y epel-release 
     yum install -y python-pip
 fi
+yum install -y python-devel libffi-devel openssl-devel
 pip install /tmp/${ANSIBLE_FULLNAME}
 rm -rf /tmp/${ANSIBLE_FULLNAME}*
