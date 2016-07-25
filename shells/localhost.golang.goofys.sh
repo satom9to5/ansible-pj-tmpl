37f59402dc79ed71291b7680a3e427b7887a02a8
satom9to5 <satom9to5@gmail.com>
2016-07-25 15:38:56 +0900

    （環境）変数名修正

--- shells/localhost.golang.goofys.sh
+++ shells/localhost.golang.goofys.sh
@@ -4,18 +4,18 @@ PLAYBOOK_DIR=$(cd $(dirname $0) && pwd)"/../"
 
 S3_BUCKET=$1
 S3_PATH=$2
-MOUNT_UID=
-MOUNT_GID=
+MOUNT_USER=
+MOUNT_GROUP=
 GOOFYS_GOPATH=${GOPATH}
 
 while getopts ":u:g:p:" OPT
 do
   case "${OPT}" in
     u)
-      MOUNT_UID=${OPTARG}
+      MOUNT_USER=${OPTARG}
       ;;
     g)
-      MOUNT_GID=${OPTARG}
+      MOUNT_GROUP=${OPTARG}
       ;;
     p)
       GOOFYS_GOPATH=${OPTARG}
@@ -41,7 +41,7 @@ fi
   -e goofys_s3_bucket=${S3_BUCKET} \
   -e goofys_s3_path=${S3_PATH} \
   -e goofys_fstab="{{ true | bool }}" \
-  -e goofys_mount_uid=${MOUNT_UID} \
-  -e goofys_mount_gid=${MOUNT_GID} \
-  -e goofys_gopath=${GOOFYS_GOPATH} \
+  -e goofys_mount_user=${MOUNT_USER} \
+  -e goofys_mount_group=${MOUNT_GROUP} \
+  -e gopath=${GOOFYS_GOPATH} \
   ${PLAYBOOK_DIR}localhost.golang.goofys.yml
